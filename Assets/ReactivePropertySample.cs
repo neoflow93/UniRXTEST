56a6c8355c362c208682b339131874688bec178b
neoflow93 <neoflow93@gmail.com>
2016-02-03 16:31:38 +0900

    aa

--- Assets/ReactivePropertySample.cs
+++ Assets/ReactivePropertySample.cs
@@ -24,6 +24,7 @@ public class ReactivePropertySample : MonoBehaviour
 {
     public Enemy ememy_ = null;
     public FruitReactiveProperty fp_2;
+    public FruitReactiveProperty fp_3;
 
     public Button fire_gun;
     public Text ememy_hp;
